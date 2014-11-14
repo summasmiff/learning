1b88e53e77e2dc372abf9e80c48679705ca8cd7d
summasmiff <jessicasummersmith@gmail.com>
2014-11-13 21:36:54 -0500

    some progress on trees

--- binary-tree.rb
+++ binary-tree.rb
@@ -19,7 +19,6 @@ module BinaryTree
 	end
 
 	class Node #use a similar node structure to a linked list
-		attr_reader :value #(read only)
 		attr_accessor :node, :left, :right #each node has two children (can be read/write)
 
 		def initialize(node) #create a node
@@ -29,7 +28,7 @@ module BinaryTree
 		end
 
 		def insert(node) #insert a new node into a preexisting tree
-			case value <=> node
+			case @node <=> node
 			when 1 then left.insert(node) #left child is always greater than
 			when -1 then right.insert(node) #right child is always less than
 			when 0 then false #cannot insert multiple nodes of the same value
@@ -41,7 +40,7 @@ module BinaryTree
 		end
 
 		def include?(node) #search our tree!
-			case value <=> node
+			case @node <=> node 
 			when 1 then left.include?(node) #go down the list
 			when -1 then right.include?(node) 
 			when 0 then true #current node
@@ -63,8 +62,6 @@ end
 
 tree = BinaryTree::Node.new(20)
 tree.left = BinaryTree::Node.new(15) 
-tree.right = BinaryTree::Node.new(25) #not displaying??
-tree.right = BinaryTree::Node.new(35)
-tree.insert(3) #not working??
-#tree.include?(15) #doesn't display anything?? how does this work
+tree.right = BinaryTree::Node.new(25) 
+tree3 = BinaryTree::Node.new(3) #you are not going where you are supposed to go
 puts tree.inspect
