4e476b6870cdf23e29f8c5a60749b0a32a30a00f
summasmiff <jessicasummersmith@gmail.com>
2014-11-14 21:28:59 -0500

    condensed quicksort somewhat

--- sorts/quicksort.rb
+++ sorts/quicksort.rb
@@ -3,20 +3,20 @@
 def quicksort(array)
 	return array if array.length <= 1 #array of 1 value is already sorted
 
-	pivot_point = array.pop#choose a value of the array, doesn't matter which
+	pivot = array.pop#choose a value of the array, doesn't matter which
 
 	left = Array.new #create the greater than array (l and r are used in binary trees, so...)
 	right = Array.new #create the lesser than array
 
 	array.each do |i| #for each value, compare to the pivot
-		if i <= pivot_point
+		if i <= pivot
 			right.push(i)
 		else
 			left.push(i)
 		end
 	end
 	#recursively sort each smaller array, then concat them together again once sorted
-	return quicksort(right).push(pivot_point) + quicksort(left)
+	return quicksort(right).push(pivot) + quicksort(left)
 end
 
 sample_array = (1..99).to_a.sample 10 #picks random 10#s between 1 and 99
