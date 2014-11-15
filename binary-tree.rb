5de91a7eb61a15b0eb60865131e86f379b7c4311
summasmiff <jessicasummersmith@gmail.com>
2014-11-14 20:13:01 -0500

    changed node to value

--- binary-tree.rb
+++ binary-tree.rb
@@ -19,42 +19,42 @@ module BinaryTree
 	end
 
 	class Node #use a similar node structure to a linked list
-		attr_accessor :node, :left, :right #each node has two children (can be read/write)
+		attr_accessor :value, :left, :right #each node has two children (can be read/write)
 
-		def initialize(node) #create a node
-			@node = node
+		def initialize(v) #create a node
+			@value = v
 			@left = MissingNode.new
 			@right = MissingNode.new
 		end
 
-		def insert(node) #insert a new node into a preexisting tree
-			case @node <=> node
-			when 1 then left.insert(node) #left child is always greater than
-			when -1 then right.insert(node) #right child is always less than
+		def insert(v) #insert a new node into a preexisting tree
+			case value <=> v
+			when 1 then left.insert(v) #left child is always greater than
+			when -1 then right.insert(v) #right child is always less than
 			when 0 then false #cannot insert multiple nodes of the same value
 			end
 		end
 
 		def inspect #display a current node and 1st gen children
-			"{#{node}::#{left.inspect}|#{right.inspect}" 
+			"{#{value}::#{left.inspect}|#{right.inspect}" 
 		end
 
-		def include?(node) #search our tree!
-			case @node <=> node 
-			when 1 then left.include?(node) #go down the list
-			when -1 then right.include?(node) 
+		def include?(v) #search our tree!
+			case value <=> v
+			when 1 then left.include?(v) #go down the list
+			when -1 then right.include?(v) 
 			when 0 then true #current node
 			end
 		end
 
 		private
 
-		def insert_left(node) #inserting children presents its own problems
-				left.insert(node) or self.left = Node.new(node)
+		def insert_left(v) #inserting children presents its own problems
+				left.insert(v) or self.left = Node.new(v)
 		end
 
-		def insert_right(node)
-				right.insert(node) or self.right = Node.new(node)
+		def insert_right(v)
+				right.insert(v) or self.right = Node.new(v)
 		end
 	end
 end
@@ -63,5 +63,5 @@ end
 tree = BinaryTree::Node.new(20)
 tree.left = BinaryTree::Node.new(15) 
 tree.right = BinaryTree::Node.new(25) 
-tree3 = BinaryTree::Node.new(3) #you are not going where you are supposed to go
+tree.left.left = BinaryTree::Node.new(3) #can i recursively insert without the .left modifier?
 puts tree.inspect 
